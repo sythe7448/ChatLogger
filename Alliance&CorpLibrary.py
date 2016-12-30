5f690454eb4fc1e99c844ce881e0b0c03050f07c
Jake Colburn <jakecolburn.sythe@gmail.com>
2016-12-30 17:32:43 -0500

    First Commit: Reads active file for updates and prints updates.

--- Alliance&CorpLibrary.py
+++ Alliance&CorpLibrary.py
@@ -1,7 +1,18 @@
 import evelink.api
 
-api = evelink.api.API()
-response = api.get('eve/AllianceList')
-for a in response.result.find('rowset').findall('row'):
-    print a.get('name')
-    print a.get('allianceID')
+charNames = 'anna niedostepny,lquid drisseg'
+b = 'lquid drisseg,'
+
+eve = evelink.eve.EVE()
+charResponse = eve.character_ids_from_names(charNames)
+#print charResponse.result
+
+ids = charResponse.result
+
+#print ids['Anna niedostepny']
+
+eve = evelink.eve.EVE()
+affResponse = eve.character_info_from_id(ids['Anna niedostepny'])
+print affResponse.result['name']
+print affResponse.result['alliance']['name'], affResponse.result['alliance']['id']
+print affResponse.result['corp']['name'], affResponse.result['corp']['id']
