8e81b953c6592451b859326ce44a05fa7df65124
Jake Colburn <jakecolburn.sythe@gmail.com>
2016-12-30 20:30:16 -0500

    First Commit: Reads active file for updates and prints updates.

--- Alliance&CorpLibrary.py
+++ Alliance&CorpLibrary.py
@@ -1,18 +1,36 @@
 import evelink.api
 
-charNames = 'anna niedostepny,lquid drisseg'
-b = 'lquid drisseg,'
+charNames = [
+    'Anna niedostepny',
+    'Lquid Drisseg'
+]
+
+charNamesString = ','.join(charNames)
 
 eve = evelink.eve.EVE()
-charResponse = eve.character_ids_from_names(charNames)
-#print charResponse.result
+charResponse = eve.character_ids_from_names(charNamesString)
+print charResponse.result
 
 ids = charResponse.result
+charIds = []
+
+# for characterName, characterId in charResponse.result.items():
+#     charIds.append(str(characterId))
+#     charIdstring = ','.join(charIds)
+#     print charIdstring
+
+for characterName, characterId in charResponse.result.items():
+    eve = evelink.eve.EVE()
+    affResponse = eve.character_info_from_id(characterId)
+    print affResponse.result['name']
+    print affResponse.result['alliance']['name'], affResponse.result['alliance']['id']
+    print affResponse.result['corp']['name'], affResponse.result['corp']['id']
+
 
 #print ids['Anna niedostepny']
 
-eve = evelink.eve.EVE()
-affResponse = eve.character_info_from_id(ids['Anna niedostepny'])
-print affResponse.result['name']
-print affResponse.result['alliance']['name'], affResponse.result['alliance']['id']
-print affResponse.result['corp']['name'], affResponse.result['corp']['id']
+# eve = evelink.eve.EVE()
+# affResponse = eve.character_info_from_id(charIdstring)
+# print affResponse.result['name']
+# print affResponse.result['alliance']['name'], affResponse.result['alliance']['id']
+# print affResponse.result['corp']['name'], affResponse.result['corp']['id']
