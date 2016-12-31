aed9d00477e73f7dfc6f120d38e9937ff547404c
Jake Colburn <jakecolburn.sythe@gmail.com>
2016-12-30 23:06:07 -0500

    First Commit: Reads active file for updates and prints updates.

--- Alliance&CorpLibrary.py
+++ Alliance&CorpLibrary.py
@@ -13,24 +13,31 @@ print charResponse.result
 
 ids = charResponse.result
 charIds = []
-
-# for characterName, characterId in charResponse.result.items():
-#     charIds.append(str(characterId))
-#     charIdstring = ','.join(charIds)
-#     print charIdstring
+shitfuck = []
 
 for characterName, characterId in charResponse.result.items():
-    eve = evelink.eve.EVE()
-    affResponse = eve.character_info_from_id(characterId)
-    print affResponse.result['name']
-    print affResponse.result['alliance']['name'], affResponse.result['alliance']['id']
-    print affResponse.result['corp']['name'], affResponse.result['corp']['id']
+    shitfuck.append(characterId)
+    charIds.append(str(characterId))
+    charIdstring = ','.join(charIds)
+    print charIdstring
+
+# for characterName, characterId in charResponse.result.items():
+#     eve = evelink.eve.EVE()
+#     affResponse = eve.character_info_from_id(characterId)
+#     print affResponse.result
+#     print affResponse.result['alliance']['name'], affResponse.result['alliance']['id']
+#     print affResponse.result['corp']['name'], affResponse.result['corp']['id']
+
+eve = evelink.eve.EVE()
+affResponse = eve.affiliations_for_characters(charIdstring)
+#print affResponse.result
+
+for item in shitfuck:
+    print affResponse.result[item]['name'], affResponse.result[item]['alliance'], affResponse.result[item]['corp']
 
 
-#print ids['Anna niedostepny']
 
-# eve = evelink.eve.EVE()
-# affResponse = eve.character_info_from_id(charIdstring)
-# print affResponse.result['name']
+
+# print affResponse.result['characters']
 # print affResponse.result['alliance']['name'], affResponse.result['alliance']['id']
 # print affResponse.result['corp']['name'], affResponse.result['corp']['id']
